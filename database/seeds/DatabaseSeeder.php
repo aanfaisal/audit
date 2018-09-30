ad9262746305aae04655579a2d636776a54c8dfd
Aan Faisal Al Fallah <aan.faisalal@gmail.com>
2018-09-30 09:47:22 +0700

    setting

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         //$this->call(UsersTableSeeder::class);
-        //$this->call(RuangSeeder::class);
+        $this->call(RuangSeeder::class);
         $this->call(BebanACSeeder::class);
-        // $this->call(BebanLainSeeder::class);
-        // $this->call(BebanMesinSeeder::class);
-        // $this->call(BebanPeneranganSeeder::class);
-        // $this->call(HItungIKESeeder::class);
-        //$this->call(ProfilSeeder::class);
+         $this->call(BebanLainSeeder::class);
+        $this->call(BebanMesinSeeder::class);
+        $this->call(BebanPeneranganSeeder::class);
+        $this->call(HItungIKESeeder::class);
+        $this->call(ProfilSeeder::class);
         
 
         //$this->call(LaratrustSeeder::class);
